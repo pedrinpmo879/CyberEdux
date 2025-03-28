6f4b529433b9e81e6415da5c0f1423dcd136d2eb
pedrinpmo879 <pedrinpmo879@gmail.com>
2025-03-28 14:01:38 -0400

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,3 +4,6 @@ with open("saida.csv", "w", newline="") as arquivo:
     escritor = csv.writer(arquivo)
     escritor.writerow(["Nome", "Idade", "Cidade"])
     escritor.writerow(["João", 30, "São Paulo"])
+
+
+aaaaaaaaaaaaaaaaaaaa
